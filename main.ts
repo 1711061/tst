5b9c85a0783c8b9932b73b0e9956465a12a2cd80
1711061 <104539673+1711061@users.noreply.github.com>
2022-04-28 09:13:01 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    basic.showString("   hello chloe")
+    basic.clearScreen()
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        # # # # #
+        # # . # #
+        # . # . #
+        # # . # #
+        # # # # #
+        `)
+    basic.showString("chloelove")
+})
